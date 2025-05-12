85bebd15c7d367772f2b60e97452fc473da8fd7b
JosephKallapari <148879457+JosephKallapari@users.noreply.github.com>
2025-05-12 03:07:03 +0100

    Update PhysioPrescribeDetails.cs

--- StrolllPhysioBackend/Models/PhysioPrescribeDetails.cs
+++ StrolllPhysioBackend/Models/PhysioPrescribeDetails.cs
@@ -1,6 +1,41 @@
-﻿namespace StrolllPhysioBackend.Common
+namespace StrolllPhysioBackend.Common
 {
-    public record PhysioPrescribeDetails
-    {
-    }
+    public record ExerciseRoutine(
+     int Id,
+     string PatientId,
+     int Age,
+     string Condition,
+     List<Exercises> PrescribedExercises
+    );
+    public record Exercises(
+                 string ExerciseName,
+     string Description,
+     int Repetitions,
+     int Sets
+        );
+    public record SessionResult (
+    
+        string SessionId,
+        string PatientId,
+        string Date,
+        int Duration,
+        List<Exercise> Exercises,
+        List<MissedExercise> MissedExercises,
+        HeartRate HeartRateBPM,
+        double ConsistencyScore,
+        int CaloriesBurned
+    );
+
+    public record Exercise (
+        string ExerciseName,
+        int Repetitions,
+        int Sets,
+        int MissedReps
+     );
+
+    public record MissedExercise(string ExerciseName,string Reason);
+
+    public record HeartRate(int Average,int Max);
+
+
 }
